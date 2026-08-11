1d8e0692fd8bdd05da0ee3329cf6aa0ed9620b46
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-08-11 11:01:47 +0900

    Add package editorial and IR visual archive data

---
+++ assets/js/portfolio-archive-data.js
@@ -0,0 +1,186 @@
+(() => {
+  const base = 'https://cdn.imweb.me/upload/S2023030963558ef55ba8e/';
+  const raw = `editorial|1|MUST IT 청년지원 프로그램 브랜드 디자인|55fafb9e86d68.png
+editorial|2|LOFT DAECHI 분양·임대 브로셔 디자인|00c3ed8a9ed71.png
+editorial|3|Deloitte 기업 브로셔 디자인|f552584d7404a.png
+editorial|4|THE ACRIUM 분양 브로셔 디자인|d3aa6c7c92e40.png
+editorial|5|한국교통대학교 교수회보 디자인|6aea8609e5358.png
+editorial|6|철도차량 잔존수명평가 프로그램 제품설명서 디자인|06c5d068e555e.png
+editorial|7|카레푸드 버거볼 리플렛 디자인|dad133ee569cd.png
+editorial|8|커피 원두 브루잉 가이드 디자인|6995262281e38.png
+editorial|9|Dal Gomz NFT 프로젝트 리플렛 디자인|771eb5b413593.png
+editorial|10|국가기술자격 안내 리플렛 디자인|b07a1b038d064.png
+editorial|11|유성 고려홍삼 제품 카탈로그 디자인|3956be280b07c.png
+editorial|12|시골농원 장지대 단감 리플렛 디자인|038a9be4245ec.png
+editorial|13|2021 원더스쿨 교육 프로그램 리플렛 디자인|bb8fd4d51c097.png
+editorial|14|Healthd 온라인몰 구매 혜택 리플렛 디자인|cd2993f6bff24.png
+editorial|15|GREENIUM OCEAN KIT 리플렛 디자인|8adaa0fe8582c.png
+editorial|16|인천광역시 창업동아리 지원사업 모집공고 디자인|092689da1fbef.png
+editorial|17|AROMACHEMI 브랜드 매거진 디자인|8663b747fd7db.png
+editorial|18|Mayer 세탁세제 제품 브로셔 디자인|a8684a4cb527d.png
+editorial|19|묵으로 남긴 기억 전시 리플렛 디자인|23c50dd186e14.png
+editorial|20|묵으로 남긴 기억 전시 브로셔 디자인|0b4f168bba796.png
+editorial|21|마에르 브로셔 디자인|96b31c1e4dd11.png
+editorial|22|딜로이트 브로셔 디자인|e57440032b901.png
+editorial|23|토이럽 브랜드 소개서 디자인|2c2b4a72c299d.png
+editorial|24|스킨핏 카탈로그 디자인|633f7bdb45c2c.png
+editorial|25|아이세이프 소개서 디자인|bc145fbd917a9.png
+editorial|26|시바로지스 소개서 디자인|cc16ff0bc12c4.png
+editorial|27|지브레인 제품 브로셔|a837e6997596e.png
+editorial|28|진천샌드 리플렛 디자인|8e1771e6bf349.png
+editorial|29|케어란스 소개서 디자인|c8b22350b3def.png
+editorial|30|바노들 브로셔 디자인|ae59da464b5b7.png
+editorial|31|에코넥트 회사소개|4a51e75295c50.png
+editorial|32|데브 회사소개서 디자인|f041bee3cf6df.png
+editorial|33|빌리브 브로셔 디자인|270edb8cd8b37.png
+editorial|34|아파트테크 서비스 소개서 디자인|690c751a4129f.png
+editorial|35|블루웨일 서비스 제안서 디자인|136809e3dd730.png
+editorial|36|리셀에클로 카탈로그 디자인|084d2f60199ec.png
+package|1|데일비|a3dd21134e190.png
+package|2|pure dust foot cream|a17bbacc353b8.jpg
+package|3|the petrichor(shower gel)|e71ad4cdcb3f5.jpg
+package|4|the petrichor(body lotion)|b33a1e8963283.jpg
+package|5|dew veil hand cream|44a7436c9b7fd.jpg
+package|6|the petrichor(cloud enzyme powder wash)|cda318c4a31d6.jpg
+package|7|the petrichor(bon matin centella calm toner)|d8e5c6dbb19a7.jpg
+package|8|rainleaf multi cica gel cream|8b7e8de4f7f43.jpg
+package|9|지리산 604 아카시아꿀|275b47a9280af.png
+package|10|벨류케어시스 바이오랩로지 스켈프 엠플|4be8cb03c1840.png
+package|11|건강한 식탁을 만들다|437ae513a6062.png
+package|12|흑홍삼절편|0394aca9774bb.png
+package|13|폴앤베이크 아메리카노|d3530af000e23.png
+package|14|위글리염|02640b2f45399.jpg
+package|15|kginseng cafe latte|8009d7c2f11b8.png
+package|16|헬씨드 관절업 옵티 MSM 2000|0a8a511f9940c.jpg
+package|17|키즈텐 밀크|f86e5cca3c4df.png
+package|18|닥터팁스|4a4097442e215.png
+package|20|리포좀 글루타치온 화이트|c112f1d56b9f5.jpg
+package|21|EASY US 일회용 마스크|d0cb5385ab52a.jpg
+package|22|헬씨드 초임계 루테인 아스타잔틴 (연세대학교 연세생활건강)|0c54dcca7996f.jpg
+package|23|불고기 버거볼 (CARFOOD)|861e1273f7ccf.png
+package|24|진천네 햇살사과 주스 (애플가든)|8b1225b976f3d.png
+package|25|The Petrichor - HYALURON DEEP COMFORT|5be56d1657a9e.png
+package|26|그대로 짜낸 순수석류 100%|6d6bafc341cee.png
+package|27|코코스틱 허니버터맛 (N&B FOOD)|a63960c8657cd.png
+package|28|산양유 단백질분말 (1plan)|7a0b2e74c3414.png
+package|29|아미율 다이어트 (Lean Body Solution)|0b3e7e14c8cee.png
+package|30|자담인 홍삼 엑기스|c717d7a6b9677.png
+package|31|오르트 문라이트 무드 젤 (orts moonlight mood gel)|b608027f48586.png
+package|32|지리산 604 야생화 스틱꿀|ea563dac262b8.png
+package|33|마에르 얼룩제거제 프로틴 (Mayer Stain Remover Protein)|d7676ec056005.png
+package|34|어린이 종합비타민 미네랄 (연세대학교 연세생활건강 키즈텐)|ee33bb5ed8ba9.png
+package|35|하이프로틴 젤리스틱 (hi High Protein)|48d616e9ba602.png
+package|36|할리스 카페 라떼 (HOLLYS CAFFE LATTE)|35aa4e64919ea.png
+package|37|할리스 리얼 벨지안 초코라떼 (HOLLYS BELGIAN CHOCO LATTE)|f01bf1af293d2.png
+package|38|푸다다 히알루론산 푸룬 젤리스틱 (비용연구소)|0b3c617453c19.png
+package|39|할리스 바닐라 딜라이트 (HOLLYS VANILLA DELIGHT)|754032418c510.jpg
+package|40|할리스 아메리카노 선물세트 (HOLLYS COFFEE AMERICANO GIFT SET)|c07cbdffbfed8.jpg
+package|41|델몬트 스카시플러스 오렌지|5c56592b848be.png
+package|42|이든셀 센텔라 콜라겐|dcf015e48dd05.jpg
+package|43|완벽하군 종합영양제|57998316663b9.jpg
+package|44|할리스 바닐라딜라이트 에디션 (HOLLYS COFFEE VANILLA DELIGHT EDITION)|6e23f0a93c66e.jpg
+package|45|퍼펙트 초이스 오가닉 프리미엄 밀 (%eat organic PREMIUM MEAL)|c01a8d2704b1a.png
+package|46|할리스 바닐라 딜라이트 스틱커피 (HOLLYS VANILLA DELIGHT)|b595d3c0e203c.jpg
+package|47|천양정 대한민국 홍삼 (OREON CHUNYANGJONG)|9b7b75553bc45.png
+package|48|케이홍삼커피 (K GINSENG COFFEE / 예천농부바이오 체천)|f2597bf3d278c.jpg
+package|49|할리스 홈카페 에디션 (HOLLYS COFFEE HOME CAFE EDITION)|84da0b3278145.jpg
+package|50|마마메 템페칩 오리지널 맛 (mamame TEMPEH CHIPS - ORIGINAL FLAVOR)|22369398f9ebf.png
+package|51|암환자를 위한 균형영양식 헬씨드 캔위드 (연세대학교 연세생활건강 healtho)|8706ec0041e42.jpg
+package|52|플리그라 강력한 숙취해결사 술루션 (Fullygra)|be3788acc12c0.png
+package|53|올데이 줄리 퓨어 호두 스크럽 (ALLDAY ZULI ALLDAY PURE HODOO SCRUB)|a433bbc057eeb.png
+package|54|오미셀 바이탈 C 비타민 앰플 (OMiCEL Vital C Vitamin Ampoule)|dc605b2233d06.png
+package|55|바다찬빛 국산 멸치 (Dried Anchovy)|c3cacc6f66b2f.jpg
+package|56|공차 우유 앙빵 플레인 (Gong cha Soft milk bread)|4aacd5a8941ec.png
+package|57|모디커스 제테 숏 브레드 쿠키 (Modicus ZETTE SHORT BREAD COOKIES)|90485119184b2.png
+package|58|광천 금매달 토굴 새우젓 / 젓갈 시리즈 (SALTED SEAFOOD)|03dcdf227d019.jpg
+package|59|밀크 플리즈! 아메리칸 베이크숍 원두/드립백 (Milk Please! American Bakeshop)|ca420f83eb362.png
+package|60|밀크 플리즈! 아메리칸 베이크숍 저지방 우유 (Milk Please! American Bakeshop low fat)|086f7cf8480c3.png
+package|61|더미엘 화이트허니 과일 꿀 시리즈|7cbf063a4c496.jpg
+package|62|자연다시 바이오 미(味)스틱 (햇살바다)|12a0689e3b2e1.png
+package|63|입암 생막걸리 (내장산 단풍회 추억과 한잔의 막걸리)|fc90a6233cf1b.png
+package|64|할리스 콜드브루 / 디카페인 커피원액|ee03232547e28.jpg
+package|65|공진왕도고|80c841527210a.png
+package|66|루가 비건 프로틴 파우더|84d22d79872f3.png
+package|67|공정무역 캡슐커피 : 기다린 캡슐 블랙|3f1f6585703ff.png
+package|68|헬리빈 로스팅 원두 브라질 세라도|c765e053d21ff.jpg
+package|69|코코스 말차 라떼 & 도넛|16d0c7140cb3e.png
+package|70|더미엘 화이트허니|78b962c307cfe.png
+package|71|몽그리 다크초코맛 / 플레인맛|409aeb15961af.png
+package|72|푸어 말차 & 캐슈 아이스크림|af39d0bef4d74.png
+package|73|센텔리안24 퍼밍 테카티놀 토너 & 앰플|c17b5925db6db.png
+package|74|미친스킨 시카덴탈치약|661660bbe3ad1.png
+package|75|Romhealth 반려견 관절 영양제|50109acb36729.png
+package|76|광천 금메달 토굴새우젓 - 오젓|3af307e0c7091.png
+package|77|유성고려홍삼 6년근 데일리 홍삼정 스틱|a5eb826bd9be8.png
+package|78|광천골드맛김|8ebcb859daf35.png
+package|79|바커 디퓨저|e1fc148a3b11e.png
+package|80|Roastingvisor 로스팅원두 / 커피|4701d6acc3112.png
+package|81|한드레 곁에 두는 쌀 10kg|8e5cebbd7ca90.png
+package|82|쥬드 뮤츄얼틱 핸드 클렌저 (JEWOOD MUTTALIC HAND CLEANSER)|df48f2bfcd15c.png
+package|83|자담인 고려홍삼절편 (JADAMIN HONEY SLICED KOREAN RED GINSENG)|cda200f87f39c.png
+package|84|르베히츠 세탁세제 (Le Behitz LAUNDRY DETERGENT)|39e17e9beaa23.png
+package|85|쥬드 데일리 케어 겔 클렌저 (JEWOOD DAILY CARE GEL CLEANSER)|a4ce747715c2e.png
+package|86|닥터팁스 에이씨카밍 & 모이스처 크림 / 토너패드|88dd96c7a8c8c.png
+package|87|드림팜 진천네 대추를 품은 노루궁뎅이 버섯즙|a9b5daf519952.png
+package|88|진천네 호박팥차|7f8b7e64df9d8.png
+package|89|금고깃집 통돼지김치찌개 밀키트|2ca9c4f2edbd0.png
+package|90|진천네 빨간 약호박즙|aa5cb2618c05e.png
+package|91|키즈텐 어린이 루테인 비타민A 플러스|a8bc08d417eba.jpg
+package|92|마예르 레이어링 세탁세제 & 섬유유연제|54c29c19ed3d7.png
+package|93|네이처메이드 요리를 위한 오브제 피라미드 플레이크 소금|9828c25f72ec8.png
+package|94|연세생활건강 헬씨드 초임계 알티지 오메가3|d151ad23769bc.jpg
+package|95|연곶감 프리미엄|6286a7622d00d.png
+package|96|동원 훈제연어 페퍼 스테이크|fd991b3b6b4f8.png
+package|97|스틸물래 (STILLMULLAE)|470db710e5184.png
+ir|1|COXSPACE (콕스스페이스)|a8a0d5954756a.png
+ir|2|kai·i (카이아이 / KAI·I Company)|96c662352809d.png
+ir|3|Gbrain (지브레인)|5b16251883916.png
+ir|4|MTHERA PHARMA (엠타라파마)|0c6512a39deeb.png
+ir|5|주식회사 에이치세컨드 (H2nd.inc)|62cd1841666ef.png
+ir|6|Eye safer (아이세이퍼)|04b9a5356aa14.png
+ir|7|SSOYOUNG (쏘영)|f44a0e3517fdd.png
+ir|8|SLEEPFOREST (슬립포레스트)|1acfd56336d54.png
+ir|9|방탈출 카페 투자제안서 (ESCAPE ROOM)|60c7d74c51f47.png
+ir|10|링닥 (RingDoc)|7090526a83014.png
+ir|11|LySeNTech (라이센텍)|6db19ca050f91.png
+ir|12|KSY-012410|bc021c252540d.png
+ir|13|인공지능 기반의 학생들 진로적성 프로그램 기획|3f6e78b622ebf.png
+ir|14|GNE|d34f090be4f2e.png
+ir|15|KINGOINVEST|9f6da187a503b.png
+ir|16|QUEST SCHOOL|9e7e5c11f0a5b.png
+ir|17|PRATER|f3bc5513dfb11.png
+ir|18|MAASFARM|9667918c6d1bf.png
+ir|19|dreamhanger (드림행어)|04350ec5ce21d.png
+ir|20|주식회사 세안 (SEAN)|c7dc742e889b3.png
+ir|21|we bring (위브링)|5afa21fd6dad8.png
+ir|22|Pajama Jam (파자마잼)|4f316a0cf4946.png
+ir|23|hurbi (허비)|ce500bc1a4fe0.png
+ir|24|NFT와 팬덤의 만나 / 덕질이 돈이 되는 팬덤 경제|963116cef016c.png
+ir|25|Riskweather (리스크웨더) / GNE Tech Holdings|182f7ce075d71.png
+ir|26|vavana (바바나)|16c21548fba64.png
+ir|27|NUDGE HEALTHCARE (넛지헬스케어)|ab1e5914cb607.png
+ir|28|(주)토마스톤 (THOMASTONE)|adcd4a36f7c25.png`;
+
+  const categoryMeta = {
+    package: { label: 'PACKAGE DESIGN', scope: 'Single Visual Archive' },
+    editorial: { label: 'EDITORIAL DESIGN', scope: 'Single Visual Archive' },
+    ir: { label: 'IR / PPT DESIGN', scope: 'Presentation Archive' }
+  };
+
+  window.NW_PORTFOLIO_ARCHIVE = raw.trim().split('\n').map((line) => {
+    const [category, number, title, file] = line.split('|');
+    const meta = categoryMeta[category];
+    return {
+      id: `archive-${category}-${number}`,
+      archive: true,
+      category,
+      filters: [category],
+      title,
+      subtitle: meta.label,
+      scope: meta.scope,
+      thumbnail: `${base}${file}`,
+      image: `${base}${file}`,
+      number: Number(number)
+    };
+  });
+})();
