6a3dd9e9368252b76b2677027b03f0025cc20e11
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-08-19 14:28:07 +0900

    Connect Nineworks Firebase web app

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,14 +1,13 @@
 // NINEWORKS Firebase Web App configuration
-// Firebase Console > Project settings > Your apps > Web app 에서 받은 값을 아래에 입력하세요.
 // Firebase Web config는 클라이언트 식별 정보이며, 실제 접근 제어는 Security Rules와 Authentication으로 처리합니다.
 
 export const firebaseConfig = Object.freeze({
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: 'AIzaSyDf0AIgR8wyHzKvohlqrGHBtnetEvsjbrc',
+  authDomain: 'nineworks-f414f.firebaseapp.com',
+  projectId: 'nineworks-f414f',
+  storageBucket: 'nineworks-f414f.firebasestorage.app',
+  messagingSenderId: '275963921149',
+  appId: '1:275963921149:web:f792008b6b275fd8827494'
 });
 
 export const firebaseConfigReady = [
