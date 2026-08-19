8d1449dbc6a545c0f39872f35aa4bed714563e80
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-08-19 14:32:58 +0900

    Refresh admin Firebase dashboard bundle

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -34,7 +34,7 @@
     }).format(now);
   }
 
-  import('./admin-firebase.js?v=20260811-1').catch((error) => {
+  import('./admin-firebase.js?v=20260819-1').catch((error) => {
     console.error('[NINEWORKS Admin] Firebase bootstrap load failed', error);
     const status = document.querySelector('.admin-status');
     if (status) status.innerHTML = '<i></i> FIREBASE LOAD ERROR';
