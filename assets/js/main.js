c8097cddf571807ecc3a173938ea2b38cf5f173c
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-09-22 01:51:17 +0900

    Bump polished navigation stylesheet cache

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,7 +46,7 @@
     typographyGuard.dataset.nwTypographyGuard = 'true';
     document.head.appendChild(typographyGuard);
   }
-  loadStyle('assets/css/nineworks-ui-system.css?v=20260922-3');
+  loadStyle('assets/css/nineworks-ui-system.css?v=20260922-4');
 
   loadScript('assets/js/seo.js?v=20260811-3');
   loadScript('assets/js/site-firebase.js?v=20260819-1');
